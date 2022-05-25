39a9604093a4f2cb2debf88a9c8d67b747d3d40e
Pawel <pawelornontowice@wp.pl>
2022-05-25 09:31:43 +0200

    Add czyPierwsza Function

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -6,8 +6,7 @@ namespace Kalkulator
     {
         static void Main(string[] args)
         {
-            NWD();
-            Pierwiastek();
+            czyPierwsza();
         }
         static void NWD()
         {
@@ -42,6 +41,29 @@ namespace Kalkulator
             double wynik = Math.Pow(a, b);
 
             Console.WriteLine("\nWynik: " + wynik);
+        }
+        static void czyPierwsza()
+        {
+            double a = 0;
+
+            Console.WriteLine("Wpisz a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i * i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    Console.WriteLine("\n" + a + " nie jest liczbą pierwszą");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\n" + a + " jest liczbą pierwszą");
+                    break;
+                }
+            }
+            
+
         }
     }
 }
